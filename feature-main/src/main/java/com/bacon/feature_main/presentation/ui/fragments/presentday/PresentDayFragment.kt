8ccc9079a3a6e575eb993a76f7941b26b1079e19
Admin <baconnkl@gmail.com>
2022-03-02 19:01:47 +0300

    PresentDayFragment and PresentDayViewModel

---
+++ feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayFragment.kt
@@ -0,0 +1,15 @@
+package com.bacon.feature_main.presentation.ui.fragments.presentday
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bacon.common.base.BaseFragment
+import com.bacon.feature_main.R
+import com.bacon.feature_main.databinding.FragmentPresentDayBinding
+
+class PresentDayFragment : BaseFragment<FragmentPresentDayBinding, PresentDayViewModel>(
+    R.layout.fragment_present_day
+) {
+    override val binding by viewBinding(FragmentPresentDayBinding::bind)
+    override val viewModel: PresentDayViewModel by viewModels()
+
+}
---
+++ feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayViewModel.kt
@@ -0,0 +1,8 @@
+package com.bacon.feature_main.presentation.ui.fragments.presentday
+
+import com.bacon.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+@HiltViewModel
+class PresentDayViewModel @Inject constructor() : BaseViewModel() {
+}
