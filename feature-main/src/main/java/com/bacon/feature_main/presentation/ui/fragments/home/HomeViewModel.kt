420bf3f0163eff16c77bf110469be18c1f490d89
Admin <baconnkl@gmail.com>
2022-03-02 18:56:59 +0300

    homeFragment And HomeViewModel

---
+++ feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.bacon.feature_main.presentation.ui.fragments.home
+
+import com.bacon.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : BaseViewModel() {
+}
