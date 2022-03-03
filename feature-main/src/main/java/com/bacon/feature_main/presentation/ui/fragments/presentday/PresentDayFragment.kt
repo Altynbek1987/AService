8460314623322ee8c6311d6404b205ac35ddca02
Admin <baconnkl@gmail.com>
2022-03-03 16:01:05 +0300

    minor c

--- feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayFragment.kt
+++ feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayFragment.kt
@@ -5,7 +5,9 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bacon.common.base.BaseFragment
 import com.bacon.feature_main.R
 import com.bacon.feature_main.databinding.FragmentPresentDayBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PresentDayFragment : BaseFragment<FragmentPresentDayBinding, PresentDayViewModel>(
     R.layout.fragment_present_day
 ) {
--- feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayViewModel.kt
+++ feature-main/src/main/java/com/bacon/feature_main/presentation/ui/fragments/presentday/PresentDayViewModel.kt
@@ -3,6 +3,6 @@ package com.bacon.feature_main.presentation.ui.fragments.presentday
 import com.bacon.common.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
 @HiltViewModel
-class PresentDayViewModel @Inject constructor() : BaseViewModel() {
-}
+class PresentDayViewModel @Inject constructor() : BaseViewModel()
