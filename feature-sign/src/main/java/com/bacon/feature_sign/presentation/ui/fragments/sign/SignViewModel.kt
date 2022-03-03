2f47244dd17f0f8e15c08e414908b646e93a52a2
Admin <baconnkl@gmail.com>
2022-03-03 16:00:04 +0300

    minor changes

--- feature-sign/src/main/java/com/bacon/feature_sign/presentation/ui/fragments/sign/SignViewModel.kt
+++ feature-sign/src/main/java/com/bacon/feature_sign/presentation/ui/fragments/sign/SignViewModel.kt
@@ -5,5 +5,4 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SignViewModel @Inject constructor() : BaseViewModel() {
-}
+class SignViewModel @Inject constructor() : BaseViewModel()
